6ce16985bbb1095afdd610650205fb97f6ec0d09
aledavidojeda <aledavojeda@gmail.com>
2026-06-24 18:51:05 -0300

    35 - Formulario de crear cuenta

--- components/auth/LoginCard.tsx
+++ components/auth/LoginCard.tsx
@@ -3,11 +3,12 @@
 import React from "react";
 import LoginCardView from "@/components/auth/LoginCardView";
 import useLoginForm from "@/hooks/useLoginForm";
+import type { RegisterProfileData } from "@/hooks/useLoginForm";
 
 type Props = {
   onSignOut: () => Promise<void>;
   onSignInWithEmail: (email: string, password: string) => Promise<void>;
-  onSignUpWithEmail: (email: string, password: string) => Promise<void>;
+  onSignUpWithEmail: (email: string, password: string, profileData?: RegisterProfileData) => Promise<void>;
   userEmail?: string | null;
 };
 
